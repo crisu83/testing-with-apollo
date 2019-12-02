932fa814bc19e67e9b5c9b82399ef2e73da19f36
Christoffer Niska <christofferniska@gmail.com>
2019-12-02 19:59:37 +0200

    Add test using MockedProvider

--- src/Hello.test.jsx
+++ src/Hello.test.jsx
@@ -1,22 +1,53 @@
+import { MockedProvider } from '@apollo/react-testing'
 import { render, wait } from '@testing-library/react'
+import gql from 'graphql-tag'
 import React from 'react'
 import ErrorProvider from './ErrorProvider'
 import Hello from './Hello'
 import LoadingProvider from './LoadingProvider'
 import SchemaProvider from './SchemaProvider'
 
-const typeDefs = `
-type Query {
-  hello: String!
-}
-`
+test('Hello component with MockedProvider', async () => {
+  const mocks = [
+    {
+      request: {
+        query: gql`
+          query {
+            hello
+          }
+        `,
+      },
+      result: {
+        data: {
+          hello: 'Hello from MockedProvider!',
+        },
+      },
+    },
+  ]
 
-test('Renders the Hello component', async () => {
+  const { getByText } = render(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <Hello />
+    </MockedProvider>,
+  )
+
+  await wait()
+
+  expect(getByText('Hello from MockedProvider!')).toBeDefined()
+})
+
+test('Hello component with SchemaProvider', async () => {
+  const typeDefs = `
+  type Query {
+    hello: String!
+  }
+  `
+
   const { getByText } = render(
     <SchemaProvider
       typeDefs={typeDefs}
       queryResolvers={{
-        hello: () => 'Hello from the schema provider!',
+        hello: () => 'Hello from SchemaProvider!',
       }}
     >
       <Hello />
@@ -25,7 +56,7 @@ test('Renders the Hello component', async () => {
 
   await wait()
 
-  expect(getByText('Hello from the schema provider!')).toBeDefined()
+  expect(getByText('Hello from SchemaProvider!')).toBeDefined()
 })
 
 test('Renders the loading state', () => {
